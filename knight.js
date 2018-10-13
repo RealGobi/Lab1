4cd3c86e560fbd4a3760be222c34dec3f6b4b708
Jimmy Svensson <jimmy888swe@gmail.com>
2018-10-13 23:04:44 +0200

    la till -2(rutaXYposition) som jag glomt innan

--- knight.js
+++ knight.js
@@ -1,18 +1,20 @@
 
+// funktionen 
+var mojligarutor = [];
+var xRutor = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h'];
+
+
 
 function springare(ruta) {
-
-    var mojligarutor = [];
-    var xRutor = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h'];
+ 
     var rutaX = xRutor.indexOf(ruta[0]) + 1;
-    var rutaY = parseInt(ruta[1]);   
+    var rutaY = parseInt(ruta[1]); 
 
-
-    var rutaXposition = [rutaX + 2, rutaX + 1, rutaX - 1].filter(function(rutaPosition){
+    var rutaXposition = [rutaX + 2, rutaX - 2, rutaX + 1, rutaX - 1].filter(function(rutaPosition){
         return (rutaPosition > 0 && rutaPosition < 9);
     })
 
-    var rutaYposition = [rutaY + 2, rutaY + 1, rutaY - 1].filter(function(rutaPosition){
+    var rutaYposition = [rutaY + 2,rutaY - 2, rutaY + 1, rutaY - 1].filter(function(rutaPosition){
         return (rutaPosition > 0 && rutaPosition < 9);
     })
 
@@ -27,6 +29,6 @@ function springare(ruta) {
     }
 
     console.log('Möjliga drag: ', mojligarutor.length);
-    return mojligarutor.length;
+    return mojligarutor;
 }
-console.log(springare('a1'));
+console.log(springare('f3'));
